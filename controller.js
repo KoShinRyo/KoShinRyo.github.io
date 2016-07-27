333dd754c33921a0c2cbd4265156574b74f883ae
Koshinryo <jpg007@ucsd.edu>
2016-07-27 16:11:18 -0700

    re-started with a new template, now fully working!

--- controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('PersonalPageApp', [])
-  .controller('PersonalPageController', function() {
-    var personalPage = this;
-    personalPage.education = [
-      {text:'University of California, San Diego'},
-      {text:'B.A. Cognitive Science with minor in Computer Science', done:true}
-    ];
-
-  });
